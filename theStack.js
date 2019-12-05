db575052a21ef16650cf66e2e184bc56fa179cd7
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2019-12-05 18:36:22 -0500

    Add files via upload

---
+++ theStack.js
@@ -0,0 +1,41 @@
+// JavaScript source code
+let paragraph;
+let button_element;
+var count = 0;
+
+function set_DOM() {
+    paragraph = document.getElementById("the_stack")
+    button_element= document.getElementById("add_to_stack")
+}
+
+function add_to_stack() {
+    message = "adding to stack, process = " + count.toString() + "<br />"
+    paragraph.innerHTML += message;
+    count += 1
+}
+
+callback = () => {
+    add_to_stack()
+    message = "Call back function called, process = " + count.toString() + "<br />"
+    paragraph.innerHTML += message
+    count += 1
+}
+
+function no_callback() {
+    message = "NO call back, process = " + count.toString() + "<br />"
+    paragraph.innerHTML += message
+    count += 1
+}
+
+function run() {
+    window.setTimeout(callback, 2 * 1000);
+    window.setTimeout(no_callback, 5000);
+}
+function setEventListeners() {
+    button_element.addEventListener('click', run)
+}
+
+window.onload = function () {
+    set_DOM()
+    setEventListeners()
+}
